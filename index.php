5f8d5edc05ec14df41beb53849bed7c02409b05b
antares667367 <logrus66@gmail.com>
2017-01-24 17:01:43 +0100

    add css

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
 <html>
   <link rel="stylesheet" href="./style.css" type="text/css">
-  <link rel="stylesheet" href="./boot.css" type="text/css">
-<table>
+<table class="vstable">
 <tr>
   <td>
-  <div class="player1" height="200px" width="100px" >
+  <div class="player1" >
   </div>
-  </td><td>
-  <div>
-    <h1>VS</h1>
-  </div>
-  </td><td>
+  </td>
+  <td>
   <div class="player2">
       </div>
   </td>
@@ -19,53 +15,53 @@
   <table class="charselect">
     <tr>
       <td>
-        <img id="anna" class="c" src='./ressources/anna/name.png'/>
+        <img id="anna" class="c x" src='./ressources/anna/name.png'/>
       </td>
       <td>
-        <img id="brian" class="c" src='./ressources/brian/name.png'/>
+        <img id="brian" class="c x" src='./ressources/brian/name.png'/>
       </td>
       <td>
-        <img id="crow" class="c" src='./ressources/crow/name.png'/>
+        <img id="crow" class="c x" src='./ressources/crow/name.png'/>
       </td>
       <td>
-        <img id="eddy" class="c" src='./ressources/eddy/name.png'/>
+        <img id="eddy" class="c x" src='./ressources/eddy/name.png'/>
       </td>
       <td>
-        <img id="gunjack" class="c" src='./ressources/gunjack/name.png'/>
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <img id="heihachi" class="c" src='./ressources/heihachi/name.png'/>
-      </td>
-      <td>
-        <img id="hwoarang" class="c" src='./ressources/hwoarang/name.png'/>
-      </td>
-      <td>
-        <img id="jin" class="c" src='./ressources/jin/name.png'/>
-      </td>
-      <td>
-        <img id="king" class="c" src='./ressources/king/name.png'/>
-      </td>
-      <td>
-        <img id="kuma" class="c" src='./ressources/kuma/name.png'/>
+        <img id="gunjack" class="c x" src='./ressources/gunjack/name.png'/>
       </td>
     </tr>
     <tr>
       <td>
-        <img id="law" class="c" src='./ressources/law/name.png'/>
+        <img id="heihachi" class="c x" src='./ressources/heihachi/name.png'/>
       </td>
       <td>
-        <img id="lei" class="c" src='./ressources/lei/name.png'/>
+        <img id="hwoarang" class="c x" src='./ressources/hwoarang/name.png'/>
       </td>
       <td>
-        <img id="mokujin" class="c" src='./ressources/mokujin/name.png'/>
+        <img id="jin" class="c x" src='./ressources/jin/name.png'/>
       </td>
       <td>
-        <img id="panda" class="c" src='./ressources/panda/name.png'/>
+        <img id="king" class="c x" src='./ressources/king/name.png'/>
       </td>
       <td>
-        <img id="yoshimitsu" class="c" src='./ressources/yoshimitsu/name.png'/>
+        <img id="kuma" class="c x" src='./ressources/kuma/name.png'/>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <img id="law" class="c x" src='./ressources/law/name.png'/>
+      </td>
+      <td>
+        <img id="lei" class="c x" src='./ressources/lei/name.png'/>
+      </td>
+      <td>
+        <img id="mokujin" class="c x" src='./ressources/mokujin/name.png'/>
+      </td>
+      <td>
+        <img id="panda" class="c x" src='./ressources/panda/name.png'/>
+      </td>
+      <td>
+        <img id="yoshimitsu" class="c x" src='./ressources/yoshimitsu/name.png'/>
       </td>
     </tr>
   </table>
@@ -86,14 +82,14 @@
     if(player1 == false && $('.player1 img').length == 0){
       $(this).on("click", function(){
         chomp.play();
-        $('.player1').append($(this).get()[0]);
+        $('.player1').append($(this).removeClass('x').addClass('vs').get()[0]);
         player1=true;
         perso1=$(this).clone().attr("id");
       });
     }else if(player2 == false && $('.player2 img').length == 0){
       $(this).on("click", function(){
         chomp.play();
-        $('.player2').append($(this).get()[0]);
+        $('.player2').append($(this).removeClass('x').addClass('vs').get()[0]);
         player2=true;
         perso2=$(this).attr("id");
         console.log(perso2);
